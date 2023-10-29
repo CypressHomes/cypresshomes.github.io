e34b772be14eb3ea05f072602b71fe7516d6380c
Kaden Franklin <kadenfranklin@hotmail.com>
2023-10-29 14:36:29 -0500

    changed fade

--- res/main.js
+++ res/main.js
@@ -1,42 +1,20 @@
-var backgroundIndex = 0;
-var fadeSpeed = 10000;
-var files = ['file1.jpg', 'file2.jpg', 'file3.jpg', 'file4.jpg'];
+var files = ['file1.jpg', 'file2.jpg', 'file3.jpg', 'file4.jpg'],
+base = "./res/background/",
+backgroundIndex = 0,
+secs = 4;
+files.forEach(function(img){
+    new Image().src = base + img; 
+});
 
-function chg_image() {
-    var currentImage = '/res/background/' + files[backgroundIndex];
-    $('.bg-image').css('background-image', 'url(' + currentImage + ')');
-
-    backgroundIndex = (backgroundIndex + 1) % files.length;
-    setTimeout(chg_image, fadeSpeed * 2);
+function backgroundSequence() {
+	window.clearTimeout();
+	for (i = 0; i < files.length; i++) {
+		setTimeout(function(){ 
+			document.documentElement.style.backgroundImage= "url(" + base + files[backgroundIndex] + ") no-repeat center center fixed";
+			document.documentElement.style.backgroundSize ="cover";
+            console.log("done");
+		if ((backgroundIndex + 1) === files.length) { setTimeout(function() { backgroundSequence()}, (secs * 1000))} else { backgroundIndex++; }			
+		}, (secs * 1000) * i)	
+	}
 }
-
-function Lights_On() {
-    var x = 0;
-    while (x < 1) {
-        x += 0.01;
-        $('.bg-image').css('opacity', x);
-        setTimeout(5);
-    }
-
-}
-
-function Lights_Off() {
-    var x = 1;
-    while (x > 0) {
-        x -= 0.01;
-        $('.bg-image').css('opacity', x);
-        setTimeout(5);
-    }
-}
-
-// Fade from black to the next image
-function fadeToNextImage() {
-    Lights_Off();
-    setTimeout(function () {
-        Lights_On();
-        chg_image();
-    }, fadeSpeed);
-}
-
-// Start the sequence
-fadeToNextImage();
+backgroundSequence();
