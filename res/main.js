4c8086b1221eeedaa3fa5acd0079b6262be067f9
Kaden Franklin <kadenfranklin@hotmail.com>
2023-10-29 16:30:59 -0500

    changed textarea

--- res/main.js
+++ res/main.js
@@ -8,14 +8,13 @@ files.forEach(function(img){
 
 
 function backgroundSequence() {
-
 	window.clearTimeout();
 	for (i = 0; i < 3; i++) {
 		setTimeout(function(){ 
             $('bg-image').css('background-image', 'url(' +  base + files[backgroundIndex] + ")"); 
-            $('bg-image').css('background-size', 'cover');
-		if ((backgroundIndex + 1) === files.length) {setTimeout(function() { backgroundSequence()}, (secs * 1000))} 
-		else { backgroundIndex++; }			
+			backgroundIndex++;
+			i++;
+		if ((backgroundIndex ) === files.length) {setTimeout(function() { backgroundSequence()}, (secs * 1000))} 
 		}, (secs * 1000) * i)	
 	}
 }
